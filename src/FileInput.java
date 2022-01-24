a48aa1c4faaf1a787b98c2454c5f7fb0f0fedb2e
sarraalqahtani <53792971+sarraalqahtani@users.noreply.github.com>
2022-01-24 16:13:42 -0500

    Initial commit

---
+++ src/FileInput.java
@@ -0,0 +1,10 @@
+public class FileInput {
+
+    public static void main(String[] args) {
+        //1) open moviesDataset.csv
+        //2) read each row and print it to the screen
+        //3) write the years of production of all movies produced after 2000 in a file
+        //4) add exception handling when needed
+    }
+
+}
